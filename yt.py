9be40d2f48aa50e9f312a7c1e82ec9da6ba21340
Romain <larcher.romain@icloud.com>
2023-02-23 20:34:39 +0100

    start

---
+++ yt.py
@@ -0,0 +1,22 @@
+#pip install pytube
+
+#import the package
+from pytube import YouTube
+
+url = input("Lien de la vidéo : \n")
+my_video = YouTube(url)
+
+print("*********************Video Title************************")
+#get Video Title
+print(my_video.title)
+
+print("********************Download video*************************")
+#get all the stream resolution for the 
+for stream in my_video.streams:
+    print(stream)
+
+#set stream resolution
+my_video = my_video.streams.get_highest_resolution()
+
+#Download video
+my_video.download()
