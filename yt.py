8993329fe918806812dfb653e6c6a45df3b54ba7
Romain <larcher.romain@icloud.com>
2023-02-24 10:30:04 +0100

    commenter ligne inutile

--- yt.py
+++ yt.py
@@ -6,14 +6,14 @@ from pytube import YouTube
 url = input("Lien de la vidéo : \n")
 my_video = YouTube(url)
 
-print("*********************Video Title************************")
-#get Video Title
-print(my_video.title)
+# print("*********************Video Title************************")
+# #get Video Title
+# print(my_video.title)
 
-print("********************Download video*************************")
-#get all the stream resolution for the 
-for stream in my_video.streams:
-    print(stream)
+# print("********************Download video*************************")
+# #get all the stream resolution for the 
+# for stream in my_video.streams:
+#     print(stream)
 
 #set stream resolution
 my_video = my_video.streams.get_highest_resolution()
